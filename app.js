5337b9e9d1ed42be887f29a69ef074e0a4a1a047
sailajasuprava <sailaja.suprava@gmail.com>
2025-07-31 19:04:49 +0530

    .env updated

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 // MIDDLEWARES
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
